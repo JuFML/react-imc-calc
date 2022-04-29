c9a44cd1c8d49d95accc3b8e389a71b27cc74359
Juliana Fernandez <julianafernandezlima@hotmail.com>
2022-04-29 06:16:31 -0300

    (feat): GridItem

--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -1,9 +1,16 @@
+import style from './GridItem.module.css'
+import upImage from '../../assets/up.png'
+import downImage from '../../assets/down.png'
+
 function GridItem({item}) {
     return (
-        <>
-            <div>{item.title}</div>
-            {/* <p>IMC está entre {item.imc[0]} e {item.imc[1]}</p> */}
-        </>
+        <div className={style.grid__main} style={{backgroundColor: item.color}}>
+            <div className={style.grid__icon}>
+                <img src={item.icon === "up" ? upImage : downImage} alt="" width= "30px"/>
+            </div>
+            <div className={style.grid__title}>{item.title}</div>
+            <p className={style.grid__paragraph}>IMC está entre {item.imc[0]} e {item.imc[1]}</p>
+        </div>
     )
 }
 
